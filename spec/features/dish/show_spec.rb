2f3faf4558d9a724a194538da626ab5b4582b9fe
Gavin Carew <gjcarew@gmail.com>
2022-09-17 21:04:36 -0700

    Refactor tests for US1

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Dish show page' do
   describe 'User story 1: When I visit a dish show page' do
     before :each do
       @chef = Chef.create!(name: 'Francoise Bonin')
-      @dish = chef.dishes.create!(name: "Mac 'n Cheese", description: 'Dangerously cheesy')
-      @macaroni = dish.ingredients.create!(name: 'Macaroni', calories: 250)
-      @milk = dish.ingredients.create!(name: 'Condensed milk', calories: 120)
-      @cheese = dish.ingredients.create!(name: 'Cheddar cheese', calories: 220)
+      @dish = @chef.dishes.create!(name: "Mac 'n Cheese", description: 'Dangerously cheesy')
+      @macaroni = @dish.ingredients.create!(name: 'Macaroni', calories: 250)
+      @milk = @dish.ingredients.create!(name: 'Condensed milk', calories: 120)
+      @cheese = @dish.ingredients.create!(name: 'Cheddar cheese', calories: 220)
       @sad_chef = Chef.create!(name: 'Ratatouille')
       sad_dish = @sad_chef.dishes.create!(name: 'Salad', description: 'Sad path salad')
-      @sad_ingredient = sad_dish.dishes.create(name: 'Lettuce', calories: 7)
+      @sad_ingredient = sad_dish.ingredients.create(name: 'Lettuce', calories: 7)
     end
 
     it "I see the dish's name and description" do
