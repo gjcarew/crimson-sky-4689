af1e861dd15f2e144e7190713f178ddea8b51f1b
Gavin Carew <gjcarew@gmail.com>
2022-09-18 09:14:57 -0700

    User story 1

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe Dish, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
   end
+
   describe "relationships" do
     it {should belong_to :chef}
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe '#instance methods' do
+    it 'prints the chefs name' do
+      chef = Chef.create!(name: 'Francoise Bonin')
+      dish = chef.dishes.create!(name: "Mac 'n Cheese", description: 'Dangerously cheesy')
+      expect(dish.chef_name).to eq(chef.name)
+    end
+  end
+
 end
